4214fd15b9536e18ac41797d43c41bb0dd2d7d0e
Ian Macaulay <ismacaul@gmail.com>
2019-12-07 15:04:01 -0700

    [Day 7.2] Implemented amplifier feedback circuit

--- 2019/7/computer.c
+++ 2019/7/computer.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct Program {
+    int* memory;
+    int length;
+    int iptr;
+    int halted;
+} Program;
+
+typedef struct Amplifier {
+    Program* program;
+    int phase;
+} Amplifier;
+
+const int NUM_AMPLIFIERS = 5;
+
+void print_memory(int* program, int length) {
+    int i;
+    for (i = 0; i < length; i++) {
+        printf("%d", program[i]);
+        if (i != length-1) {
+            printf(",");
+        }
+    }
+}
+
 int count_memory(char* input) {
     int i, count;
     i = 0;
@@ -41,14 +65,54 @@ void init_memory(char* data, int** memory, int* length) {
     *memory = mem;
 }
 
-void print_memory(int* program, int length) {
-    int i;
-    for (i = 0; i < length; i++) {
-        printf("%d", program[i]);
-        if (i != length-1) {
-            printf(",");
-        }
+Program* create_program(int* memory, int length) {
+    Program* program = (Program*)malloc(sizeof(Program));
+    program->memory = (int*)malloc(sizeof(int) * length);
+    memcpy(program->memory, memory, sizeof(int) * length);
+    program->length = length;
+    program->iptr = 0;
+    program->halted = 0;
+    return program;
+}
+
+void free_program(Program* program) {
+    free(program->memory);
+    free(program);
+}
+
+Amplifier** create_amplifiers(int* memory, int length) {
+    Amplifier** amplifiers = (Amplifier**)malloc(sizeof(Amplifier*) * NUM_AMPLIFIERS);
+    for (int i = 0; i < NUM_AMPLIFIERS; i++) {
+        Amplifier* amp = (Amplifier*)malloc(sizeof(Amplifier));
+        amp->program = create_program(memory, length);
+        amp->phase = -1;
+
+        amplifiers[i] = amp;
     }
+
+    return amplifiers;
+}
+
+void free_amplifiers(Amplifier** amplifiers) {
+    for (int i = 0; i < NUM_AMPLIFIERS; i++) {
+        Amplifier* amp = amplifiers[i];
+        free_program(amp->program);
+        free(amp);
+    }
+
+    free(amplifiers);
+}
+
+Amplifier** copy_amplifiers(Amplifier** amplifiers) {
+    Amplifier** copy = (Amplifier**)malloc(sizeof(Amplifier*) * NUM_AMPLIFIERS);
+    for (int i = 0; i < NUM_AMPLIFIERS; i++) {
+        Amplifier* amp = (Amplifier*)malloc(sizeof(Amplifier));
+        amp->program = create_program(amplifiers[i]->program->memory, amplifiers[i]->program->length);
+        amp->phase = amplifiers[i]->phase;
+        copy[i] = amp;
+    }
+
+    return copy;
 }
 
 int parse_instruction(int opcode) {
@@ -82,8 +146,13 @@ int get_param_value(int mode, int param, int* memory) {
     return param;
 }
 
-int run(int* memory, int* input) {
-    int iptr = 0;
+void run(Program* program, int* input, int* output) {
+    if (program->halted) {
+        return;
+    }
+
+    int* memory = program->memory;
+    int iptr = program->iptr;
     while(1) {
         int opcode = memory[iptr];
         int instruction = parse_instruction(opcode);
@@ -119,9 +188,10 @@ int run(int* memory, int* input) {
             // 4 <param>
             int mode = parse_mode(opcode, 0);
             int value = get_param_value(mode, memory[iptr+1], memory);
-            return value;
+            *output = value;
 
             iptr += 2;
+            break;
         } else if (instruction == 5) {
             // jump-if-true
             // 5 <param> <param>
@@ -163,29 +233,60 @@ int run(int* memory, int* input) {
 
             iptr += 4;
         } else if (instruction == 99) {
+            program->halted = 1;
             break;
         }
     }
+
+    program->iptr = iptr;
 }
 
-int compute_thruster_signal(int* memory, int phase[5]) {
-    int signal = 0;
-    for (int i = 0; i < 5; i++) {
-        int input[2] = {phase[i], signal};
-        signal = run(memory, input);
+int run_pipeline(Amplifier** amplifiers, int input) {
+    int output = input;
+    for (int i = 0; i < NUM_AMPLIFIERS; i++) {
+        Amplifier* amp = amplifiers[i];
+        Program* program = create_program(amp->program->memory, amp->program->length);
+        int input[] = {amp->phase, output};
+
+        run(program, input, &output);
+        free_program(program);
     }
-    return signal;
+    return output;
 }
 
-int find_max_signal(int* memory, int length, int input_signal, int phase, int* remaining_phases, int num_remaining_phases) {
-    int* mem = (int*)malloc(sizeof(int) * length);
-    memcpy(mem, memory, sizeof(int) * length);
-    int input[] = {phase, input_signal};
-    int signal = run(mem, input);
-    free(mem);
+int run_pipeline_feedback(Amplifier** amplifiers, int input) {
+    int output = 0;
+    int loops = 0;
 
+    Amplifier** amplifiers_copy = copy_amplifiers(amplifiers);
+    while(1) {
+        for (int i = 0; i < NUM_AMPLIFIERS; i++) {
+            Amplifier* amp = amplifiers_copy[i];
+            if (loops == 0) {
+                int input[] = {amp->phase, output};
+                run(amp->program, input, &output);
+            } else {
+                int input[] = {output};
+                run(amp->program, input, &output);
+            }
+        }
+
+        if (amplifiers_copy[NUM_AMPLIFIERS-1]->program->halted) {
+            break;
+        }
+
+        loops++;
+    }
+
+    free_amplifiers(amplifiers_copy);
+    return output;
+}
+
+int find_max_signal(Amplifier** amplifiers, int phase, int* remaining_phases, int num_remaining_phases, int(*runner)(Amplifier**, int)) {
+    int idx = 4 - num_remaining_phases;
+    amplifiers[idx]->phase = phase;
     if (num_remaining_phases == 0) {
-        return signal;
+        return runner(amplifiers, 0);
     }
 
     int max = 0;
@@ -201,7 +302,7 @@ int find_max_signal(int* memory, int length, int input_signal, int phase, int* r
             }
         }
 
-        int s = find_max_signal(memory, length, signal, phase, next_phases, num_remaining_phases - 1);
+        int s = find_max_signal(amplifiers, phase, next_phases, num_remaining_phases - 1, runner);
         if (s > max) {
             max = s;
         }
@@ -211,37 +312,51 @@ int find_max_signal(int* memory, int length, int input_signal, int phase, int* r
     return max;
 }
 
-int find_max_thruster_signal(int* initial_memory, int length) {
-
+int find_max_thruster_signal(Amplifier** amplifiers, int phase_start, int(*runner)(Amplifier**, int)) {
     int max = 0;
-    for(int i = 0; i < 5; i++) {
-        int remaining_phases[4];
+    for(int i = phase_start; i < phase_start + NUM_AMPLIFIERS; i++) {
+        int remaining_phases[NUM_AMPLIFIERS-1];
         int idx = 0;
-        for (int j = 0; j < 5; j++) {
-            if (j != i) {
-                remaining_phases[idx] = j;
+        for (int j = 0; j < NUM_AMPLIFIERS; j++) {
+            if (j != i-phase_start) {
+                remaining_phases[idx] = phase_start + j;
                 idx++;
             }
         }
-
-        int signal = find_max_signal(initial_memory, length, 0, i, remaining_phases, 4);
+        int signal = find_max_signal(amplifiers, i, remaining_phases, 4, runner);
         if (signal > max) {
             max = signal;
         }
     }
+
     return max;
 }
-
 
 void run_test_case(char* input, int expected) {
     int* memory = NULL;
     int length;
 
     init_memory(input, &memory, &length);
+    Amplifier** amplifiers = create_amplifiers(memory, length);
 
-    int signal = find_max_thruster_signal(memory, length);
+    int signal = find_max_thruster_signal(amplifiers, 0, &run_pipeline);
     printf("(%d) should be (%d)\n", signal, expected);
 
+    free_amplifiers(amplifiers);
+    free(memory);
+}
+
+void run_feeback_test_case(char* input, int expected) {
+    int* memory = NULL;
+    int length;
+
+    init_memory(input, &memory, &length);
+    Amplifier** amplifiers = create_amplifiers(memory, length);
+
+    int signal = find_max_thruster_signal(amplifiers, 5, &run_pipeline_feedback);
+    printf("(%d) should be (%d)\n", signal, expected);
+
+    free_amplifiers(amplifiers);
     free(memory);
 }
 
@@ -262,6 +377,16 @@ int main() {
         int expected = 65210;
         run_test_case(input, expected);
     }
+    {
+        char* input = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
+        int expected = 139629729;
+        run_feeback_test_case(input, expected);
+    }
+    {
+        char* input = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10";
+        int expected = 18216;
+        run_feeback_test_case(input, expected);
+    }
 
     FILE* file;
     file = fopen("input.txt", "r");
@@ -273,12 +398,22 @@ int main() {
     char* buf = NULL;
     size_t len = 0;
     getline(&buf, &len, file);
+    fclose(file);
 
-    int* initial_memory = NULL;
+    int* memory = NULL;
     int length;
-    init_memory(buf, &initial_memory, &length);
-    int signal = find_max_thruster_signal(initial_memory, length);
-    printf("Found max thruster signal: %d\n", signal);
+    init_memory(buf, &memory, &length);
+    free(buf);
 
-    free(initial_memory);
+    Amplifier** amplifiers = create_amplifiers(memory, length);
+    int signal = find_max_thruster_signal(amplifiers, 0, &run_pipeline);
+    printf("Found max thruster signal: %d\n", signal);
+    free_amplifiers(amplifiers);
+
+    amplifiers = create_amplifiers(memory, length);
+    signal = find_max_thruster_signal(amplifiers, 5, &run_pipeline_feedback);
+    printf("Found max thruster signal using feedback pipeline: %d\n", signal);
+    free_amplifiers(amplifiers);
+
+    free(memory);
 }
